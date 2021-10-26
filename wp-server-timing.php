071cbf5edfaf3ff801d06f98725e690571dfc7be
ocReaper <ocreaper@gmail.com>
2021-10-26 19:49:22 +0200

    refactor plugin static

--- wp-server-timing.php
+++ wp-server-timing.php
@@ -10,131 +10,138 @@
  Text Domain: wp-server-timing
  */
 
-class WPServerTiming
-{
-    protected $timebootstrap = 0;
-    protected $timehttprequest = 0;
-    protected $timeplugins;
-    protected $timesetuptheme;
-    protected $timeaftersetuptheme;
+class WP_Server_Timing {
 
-    public function __construct()
-    {
-        add_filter('http_request_args', array($this, 'add_time_start_to_http_request'), 10, 3);
+	protected int $time_bootstrap = 0;
 
-        add_filter('http_api_debug', array($this, 'calculate_duration_after_http_request'), 10, 5);
+	protected int $time_http_request = 0;
 
-        add_action('wp_loaded', array($this, 'add_time_metric_to_wp_loaded'), PHP_INT_MAX);
+	protected int $time_plugins;
 
-        add_action('plugins_loaded', array($this, 'add_time_metric_to_plugins_loaded'), PHP_INT_MAX);
+	protected int $time_setup_theme;
 
-        add_action('setup_theme', array($this, 'add_time_metric_to_setup_theme'), PHP_INT_MAX);
+	protected int $time_after_setup_theme;
 
-        add_action('after_setup_theme', array($this, 'add_time_metric_to_after_setup_theme'), PHP_INT_MAX);
+	public function __construct() {
+		add_filter( 'http_request_args', array( $this, 'add_time_start_to_http_request' ), 10, 3 );
 
-        add_action('send_headers', array($this, 'start_collecting_response'), -1);
+		add_filter( 'http_api_debug', array( $this, 'calculate_duration_after_http_request' ), 10, 5 );
 
-        add_action('send_headers', array( $this, 'start_collecting_response'), -1);
+		add_action( 'wp_loaded', array( $this, 'add_time_metric_to_wp_loaded' ), PHP_INT_MAX );
 
-        add_action('wp_footer', array($this, 'flush_content_and_add_timig_headers'), PHP_INT_MAX);
-    }
+		add_action( 'plugins_loaded', array( $this, 'add_time_metric_to_plugins_loaded' ), PHP_INT_MAX );
 
-    public function add_time_start_to_http_request($args, $url)
-    {
-        $args['time_start'] = microtime(true);
+		add_action( 'setup_theme', array( $this, 'add_time_metric_to_setup_theme' ), PHP_INT_MAX );
 
-        return $args;
-    }
+		add_action( 'after_setup_theme', array( $this, 'add_time_metric_to_after_setup_theme' ), PHP_INT_MAX );
 
-    public function calculate_duration_after_http_request($response, $type, $class, $args, $url)
-    {
-        if ($type !== 'response') {
-            return;
-        }
+		add_action( 'send_headers', array( $this, 'start_collecting_response' ), - 1 );
 
-        $args['time_stop'] = microtime(true);
+		add_action( 'send_headers', array( $this, 'start_collecting_response' ), - 1 );
 
-        $args['duration'] = $args['time_stop'] - $args['time_start'];
-        $args['duration'] *= 1000;
+		add_action( 'wp_footer', array( $this, 'flush_content_and_add_timing_headers' ), PHP_INT_MAX );
+	}
 
-        $this->timehttprequest += $args['duration'];
-    }
+	public function add_time_start_to_http_request( $args, $url ): array {
+		$args['time_start'] = microtime( true );
 
-    public function calculate_query_time()
-    {
-        global $wpdb;
+		return $args;
+	}
 
-        $timewpquery = 0;
+	public function calculate_duration_after_http_request( $response, $type, $class, $args, $url ) {
+		if ( $type !== 'response' ) {
+			return;
+		}
 
-        if (!empty($wpdb->queries)) {
-            foreach ($wpdb->queries as $q) {
-                list($query, $elapsed, $debug) = $q;
+		$args['time_stop'] = microtime( true );
 
-                $timewpquery += $elapsed;
-            }
-        }
+		$args['duration'] = $args['time_stop'] - $args['time_start'];
+		$args['duration'] *= 1000;
 
-        return $timewpquery * 1000;
-    }
+		$this->time_http_request += $args['duration'];
+	}
 
-    public function add_time_metric_to_wp_loaded()
-    {
-        $this->timebootstrap = microtime(true);
-    }
+	public function calculate_query_time(): int {
+		global $wpdb;
 
-    public function add_time_metric_to_plugins_loaded()
-    {
-        $this->timeplugins = microtime(true);
-    }
+		$time_wp_query = 0;
 
-    public function add_time_metric_to_setup_theme()
-    {
-        $this->timesetuptheme = microtime(true);
-    }
+		if ( ! empty( $wpdb->queries ) ) {
+			foreach ( $wpdb->queries as $q ) {
+				list( $query, $elapsed, $debug ) = $q;
 
-    public function add_time_metric_to_after_setup_theme()
-    {
-        $this->timeaftersetuptheme = microtime(true);
-    }
+				$time_wp_query += $elapsed;
+			}
+		}
 
-    public function start_collecting_response()
-    {
-        ob_start();
-    }
+		return $time_wp_query * 1000;
+	}
 
-    public function generate_timing_header( $name, $description, $startTime, $endTime ) {
-        if (!isset($endTime)) {
-            $endTime = microtime(true );
-        }
+	public function add_time_metric_to_wp_loaded() {
+		$this->time_bootstrap = microtime( true );
+	}
 
-        $timeSpent = ( $endTime - $startTime ) * 1000;
+	public function add_time_metric_to_plugins_loaded() {
+		$this->time_plugins = microtime( true );
+	}
 
-        return "$name;desc=\"$description\";dur=\"$timeSpent\"";
-    }
+	public function add_time_metric_to_setup_theme() {
+		$this->time_setup_theme = microtime( true );
+	}
 
-    public function flush_content_and_add_timig_headers()
-    {
-        global $timestart;
+	public function add_time_metric_to_after_setup_theme() {
+		$this->time_after_setup_theme = microtime( true );
+	}
 
-        $requestStart = floatval($_SERVER['REQUEST_TIME_FLOAT'] ?? $timestart);
-        $bootstrapTime = 'bootstrap;desc="WP setup";dur=' . (($timestart - $requestStart) * 1000);
-        $wpLoadTime = 'wpload;desc="Non template logic";dur=' . (($this->timebootstrap - $this->timeaftersetuptheme) * 1000);
-        $appTime = 'html;desc="Template processing";dur=' . ((microtime(true) - $this->timebootstrap) * 1000);
-        $apiTime = 'api;desc="API request processing";dur=' . $this->timehttprequest;
-        $dbTime = 'db;desc="WPDB";dur=' . $this->calculate_query_time();
-        $globalQueryTime = 'globals;desc="Global query setup";dur=' . (($this->timesetuptheme - $this->timeplugins) * 1000);
-        $pluginTime = 'plugins;desc="Plugins loaded";dur=' . (($this->timeplugins - $timestart) * 1000);
-        $themeTime = 'theme;desc="Theme loaded";dur=' . (($this->timeaftersetuptheme - $this->timesetuptheme) * 1000);
-        $totalTime = 'total;desc="Total application run time";dur=' . ((microtime(true) - $requestStart) * 1000);
-        $data = [$bootstrapTime, $dbTime, $globalQueryTime, $pluginTime, $themeTime, $wpLoadTime, $apiTime, $appTime, $totalTime];
-        $data = apply_filters( 'wp_server_timing_flush_timing_headers', $data );
+	public function start_collecting_response() {
+		ob_start();
+	}
 
-        header('Server-Timing: ' . implode(', ', $data));
-        ob_end_flush();
-    }
+	public function generate_timing_header( $name, $description, $start_time, $end_time = null ): string {
+		if ( is_null( $end_time ) ) {
+			$end_time = microtime( true );
+		}
+
+		$time_spent = ( $end_time - $start_time ) * 1000;
+
+		return "$name;desc=\"$description\";dur=\"$time_spent\"";
+	}
+
+	public function flush_content_and_add_timing_headers() {
+		global $timestart;
+
+		$requestStart    = floatval( $_SERVER['REQUEST_TIME_FLOAT'] ?? $timestart );
+		$bootstrapTime   = $this->generate_timing_header( 'bootstrap', 'WP setup', $requestStart, $timestart );
+		$wpLoadTime      = $this->generate_timing_header( 'wpload', 'Non template logic', $this->time_after_setup_theme, $this->time_bootstrap );
+		$appTime         = $this->generate_timing_header( 'html', 'Template processing', $this->time_bootstrap );
+		$apiTime         = 'api;desc="API request processing";dur=' . $this->time_http_request;
+		$dbTime          = 'db;desc="WPDB";dur=' . $this->calculate_query_time();
+		$globalQueryTime = $this->generate_timing_header( 'globals', 'Global query setup', $this->time_plugins, $this->time_setup_theme );
+		$pluginTime      = $this->generate_timing_header( 'plugins', 'Plugins loaded', $timestart, $this->time_plugins );
+		$themeTime       = $this->generate_timing_header( 'theme', 'Theme loaded', $this->time_setup_theme, $this->time_after_setup_theme );
+		$totalTime       = $this->generate_timing_header( 'total', 'Total application run time', $requestStart );
+
+		$data = apply_filters(
+			'wp_server_timing_flush_timing_headers',
+			[
+				$bootstrapTime,
+				$dbTime,
+				$globalQueryTime,
+				$pluginTime,
+				$themeTime,
+				$wpLoadTime,
+				$apiTime,
+				$appTime,
+				$totalTime
+			]
+		);
+
+		header( 'Server-Timing: ' . implode( ', ', $data ) );
+		ob_end_flush();
+	}
 
 }
 
-global $wpServerTiming;
+global $wp_server_timing;
 
-$wpServerTiming= new WPServerTiming();
+$wp_server_timing = new WP_Server_Timing();
